22ce2c3c0708e49f88c75dc2d0103b8c811a195b
Patryk097 <patryk.kowalczyk097@gmail.com>
2021-01-07 12:36:26 +0100

    Zadanie 2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 'use strict';
 console.log('Działa');
-document.addEventListener('DOMContentLoaded', function() {
-document.querySelector(".for-dropdown a").addEventListener("mouseover", menuMouseover);
-document.querySelector(".for-dropdown a").addEventListener("mouseout", menuMouseout);
-function menuMouseover(){
-	document.querySelector('.dropdown').style.display = "block";
-}
+document.addEventListener('DOMContentLoaded', function(){
 
-function menuMouseout(){
-	document.querySelector('.dropdown').style.display = "none";
-}
+document.querySelector(".for-dropdown a").addEventListener("mouseover", function menuMouseover(){
+	document.querySelector('.dropdown').style.display = "block";
 });
+
+document.querySelector(".for-dropdown a").addEventListener("mouseout", function menuMouseout(){
+	document.querySelector('.dropdown').style.display = "none";
+});
+
+
+  var buttons = document.querySelectorAll('.read-more');
+
+  function showHide(){
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
+
+  for (var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', showHide);
+  }
+
+});
+
