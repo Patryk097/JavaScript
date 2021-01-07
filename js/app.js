913272dbcae15c3de02ccf7e22ab373e99ecbeb4
Patryk097 <patryk.kowalczyk097@gmail.com>
2021-01-07 11:26:13 +0100

    Zadanie 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+'use strict';
 console.log('Działa');
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector(".for-dropdown a").addEventListener("mouseover", menuMouseover);
+document.querySelector(".for-dropdown a").addEventListener("mouseout", menuMouseout);
+function menuMouseover(){
+	document.querySelector('.dropdown').style.display = "block";
+}
+
+function menuMouseout(){
+	document.querySelector('.dropdown').style.display = "none";
+}
+});
